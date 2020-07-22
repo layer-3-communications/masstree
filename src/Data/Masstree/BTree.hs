2272a24e7dcfcf68ecab37266122618e83584a33
Andrew Martin <andrew.thaddeus@gmail.com>
2020-07-22 15:49:29 -0400

    Specialize Either to avoid boxing a Word64

--- src/Data/Masstree/BTree.hs
+++ src/Data/Masstree/BTree.hs
@@ -64,20 +64,24 @@ lookup Branch{keys,children} k = lookup (Arr.index children $ findChild keys k) 
 insert :: BTree v -> Word64 -> v -> BTree v
 insert t k v = runIdentity $ upsertF (pure . const v) t k
 
+data Result v
+  = Split !(BTree v) {-# UNPACK #-} !Word64 !(BTree v)
+  | Ok !(BTree v)
+
 upsertF :: forall f v. (Functor f) => (Maybe v -> f v) -> BTree v -> Word64 -> f (BTree v)
 upsertF f root k = go root <&> \case
-  Right root' -> root'
-  Left (left, keyM, right) -> Branch
+  Ok root' -> root'
+  Split left keyM right -> Branch
     -- unsafeMinKey is ok because an empty tree will never split a node on insert, and therefore never make it to this branch
     { keys = Arr.singleton keyM
     , children = Arr.doubleton left right
     }
   where
-  go :: BTree v -> f (Either (BTree v, Word64, BTree v) (BTree v))
+  go :: BTree v -> f (Result v)
   go Leaf{keys,values} = case findInsRep keys k of
     -- replace
     Right i -> Arr.modifyAtF (f . Just) values i <&> \values' ->
-      Right Leaf {keys, values = values' }
+      Ok Leaf {keys, values = values' }
     -- insert
     -- TODO for now I'm just inserting first and splitting later
     -- I could avoid some memory copying if I figured out destinations ahead-of-time
@@ -85,7 +89,7 @@ upsertF f root k = go root <&> \case
       let keys' = Arr.insertAt keys i k
           values' = Arr.insertAt values i v
        in if Arr.size values' <= fanout
-          then Right
+          then Ok
             Leaf {keys = keys', values = values'}
           else
             let at = (Arr.size keys `div` 2) + 1
@@ -94,18 +98,18 @@ upsertF f root k = go root <&> \case
                 (valuesL, valuesR) = Arr.splitAt values' at
                 left = Leaf{keys = keysL, values = valuesL}
                 right = Leaf{keys = keysR, values = valuesR}
-             in Left (left, keyM, right)
+             in Split left keyM right
   go Branch{keys,children} =
     let i = findChild keys k
      in go (Arr.index children i) <&> \case
-        Left (left, keyM, right) ->
+        Split left keyM right ->
           -- TODO for now I'm just inserting first and splitting later
           -- I could avoid some memory copying if I figured out destinations ahead-of-time
           let keys' = Arr.insertAt keys i keyM
               -- TODO replace after insertAt? what hilarious amounts of copying!
               children' = Arr.replaceAt (Arr.insertAt children i left) (i + 1) right
            in if Arr.size children' <= fanout
-              then Right
+              then Ok
                 Branch {keys = keys', children = children'}
               else
                 let at = (Arr.size children `div` 2) + 1
@@ -115,8 +119,8 @@ upsertF f root k = go root <&> \case
                     (childrenL, childrenR) = Arr.splitAt children' at
                     left' = Branch {keys = keysL', children = childrenL}
                     right' = Branch {keys = keysR', children = childrenR}
-                 in Left (left', keyM', right')
-        Right child' -> Right Branch
+                 in Split left' keyM' right'
+        Ok child' -> Ok Branch
           { keys -- don't have to update the keys, since the new children
                  -- won't have a key smaller than it already had
           , children = Arr.replaceAt children i child'
